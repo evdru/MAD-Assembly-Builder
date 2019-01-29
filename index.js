84e80ac6d1896e45bef686fe0b88fb77a4266991
Evan Russell <edr62@nau.edu>
2019-01-28 20:24:09 -0700

    Added basic drag and drop functionality for Component and Place creation

--- index.js
+++ index.js
@@ -39,5 +39,31 @@ const mainMenuTemplate = [
 	}
 ];
 
+// if mac, add empty object to menu
+if(process.platform == 'darwin'){
+	mainMenuTemplate.unshift({});
+}
+
+// Add developer tools item if not in production
+if(process.env.NODE_ENV !== 'production'){
+	mainMenuTemplate.push({
+		label: 'Developer Tools',
+		submenu:[
+			{
+				label: 'Toggle DevTools',
+				// Keyboard shortcuts for DevTools
+				accelerator: process.platform == 'darwin' ? 'Command+I' :
+				'Ctrl+I',
+				click(item, focusedWindow){
+					focusedWindow.toggleDevTools();
+				}
+			},
+			{
+				role: 'reload'
+			}
+		]
+	});
+}
+
 // Listen for app to be ready
 app.on('ready', boot);
