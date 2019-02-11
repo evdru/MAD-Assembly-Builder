2347ffc187d2f281bb4f97fd33eb03b3c958186e
Evan Russell <edr62@nau.edu>
2019-02-11 13:58:04 -0700

    Added two additional points to dependency connection line

--- JS/connection.js
+++ JS/connection.js
@@ -1,45 +1,80 @@
 // Add new connection function, should only be called by provide depedency stub
 function addNewConnection(provide_component_obj, provide_source_obj, provide_stub_konva, provide_component_group, use_component_obj, use_source_obj, use_stub_konva, use_component_group) {
 
+    var provide_offset = 20;
+    var use_offset = -20;
+    var midpoint_x = ((provide_stub_konva.getAbsolutePosition().x + provide_offset) + (use_stub_konva.getAbsolutePosition().x + use_offset)) / 2;
+
     var connection = new Konva.Line({
-        points: [provide_stub_konva.getAbsolutePosition().x, provide_stub_konva.getAbsolutePosition().y, use_stub_konva.getAbsolutePosition().x, use_stub_konva.getAbsolutePosition().y],
+        points: [provide_stub_konva.getAbsolutePosition().x + provide_offset, 
+                 provide_stub_konva.getAbsolutePosition().y, 
+                 midpoint_x,
+                 provide_stub_konva.getAbsolutePosition().y,
+                 midpoint_x,
+                 use_stub_konva.getAbsolutePosition().y,
+                 use_stub_konva.getAbsolutePosition().x + use_offset, 
+                 use_stub_konva.getAbsolutePosition().y],
         stroke: 'black',
         strokeWidth: 1,
         name: 'connection',
         lineCap: 'round',
         lineJoin: 'round',
-        tension : 1
+        tension : 0
     });
 
     // when the provide dependency moves
     provide_stub_konva.on('xChange yChange', (e) => {
-        connection.setPoints([provide_stub_konva.getAbsolutePosition().x, 
+        // recalculate the midpoint
+        midpoint_x = ((provide_stub_konva.getAbsolutePosition().x + provide_offset) + (use_stub_konva.getAbsolutePosition().x + use_offset)) / 2;
+        connection.setPoints([provide_stub_konva.getAbsolutePosition().x + provide_offset, 
                               provide_stub_konva.getAbsolutePosition().y, 
-                              use_stub_konva.getAbsolutePosition().x, 
+                              midpoint_x,
+                              provide_stub_konva.getAbsolutePosition().y,
+                              midpoint_x,
+                              use_stub_konva.getAbsolutePosition().y,
+                              use_stub_konva.getAbsolutePosition().x + use_offset, 
                               use_stub_konva.getAbsolutePosition().y]);
     });
 
     // when the provide component moves
     provide_component_group.on('xChange yChange', (e) => {
-        connection.setPoints([provide_stub_konva.getAbsolutePosition().x, 
+        // recalculate the midpoint
+        midpoint_x = ((provide_stub_konva.getAbsolutePosition().x + provide_offset) + (use_stub_konva.getAbsolutePosition().x + use_offset)) / 2;
+        connection.setPoints([provide_stub_konva.getAbsolutePosition().x + provide_offset, 
                               provide_stub_konva.getAbsolutePosition().y, 
-                              use_stub_konva.getAbsolutePosition().x, 
+                              midpoint_x,
+                              provide_stub_konva.getAbsolutePosition().y,
+                              midpoint_x,
+                              use_stub_konva.getAbsolutePosition().y,
+                              use_stub_konva.getAbsolutePosition().x + use_offset,
                               use_stub_konva.getAbsolutePosition().y]);
     });
 
     // when the provide dependency moves
     use_stub_konva.on('xChange yChange', (e) => {
-        connection.setPoints([provide_stub_konva.getAbsolutePosition().x,
+        // recalculate the midpoint
+        midpoint_x = ((provide_stub_konva.getAbsolutePosition().x + provide_offset) + (use_stub_konva.getAbsolutePosition().x + use_offset)) / 2;
+        connection.setPoints([provide_stub_konva.getAbsolutePosition().x + provide_offset, 
                               provide_stub_konva.getAbsolutePosition().y, 
-                              use_stub_konva.getAbsolutePosition().x, 
+                              midpoint_x,
+                              provide_stub_konva.getAbsolutePosition().y,
+                              midpoint_x,
+                              use_stub_konva.getAbsolutePosition().y,
+                              use_stub_konva.getAbsolutePosition().x + use_offset, 
                               use_stub_konva.getAbsolutePosition().y]);
     });
 
     // when the provide component moves
     use_component_group.on('xChange yChange', (e) => {
-        connection.setPoints([provide_stub_konva.getAbsolutePosition().x, 
+        // recalculate the midpoint
+        midpoint_x = ((provide_stub_konva.getAbsolutePosition().x + provide_offset) + (use_stub_konva.getAbsolutePosition().x + use_offset)) / 2;
+        connection.setPoints([provide_stub_konva.getAbsolutePosition().x + provide_offset, 
                               provide_stub_konva.getAbsolutePosition().y, 
-                              use_stub_konva.getAbsolutePosition().x, 
+                              midpoint_x,
+                              provide_stub_konva.getAbsolutePosition().y,
+                              midpoint_x,
+                              use_stub_konva.getAbsolutePosition().y,
+                              use_stub_konva.getAbsolutePosition().x + use_offset, 
                               use_stub_konva.getAbsolutePosition().y]);
     });
 
