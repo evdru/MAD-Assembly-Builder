33a77c12c2d4e5ce8d19cb94c4166ee11dbcd6bd
Evan Russell <edr62@nau.edu>
2019-02-12 21:43:28 -0700

    Connections can only be made if both Provide and Use are of the same type

--- JS/dependency.js
+++ JS/dependency.js
@@ -3,7 +3,6 @@ function addNewServiceDependency(component, source_element, source_obj, componen
     var offset;
     var add;
     var stub_x;
-    var depedency_name;
 
     // provide connection going right of a place
     if(source_obj.type == 'Place') {
@@ -14,7 +13,6 @@ function addNewServiceDependency(component, source_element, source_obj, componen
         offset = component.getWidth();
         add = 20;
         stub_x = 0;
-        depedency_name = dependency_obj.type + " Provide Dependency from " + source_obj.name;
     } else if (source_obj.type == 'Transition') {
         // create the dependency object
         var dependency_obj = new Dependency('USE', "Dependency_" + (component_obj.dependency_list.length + 1));
@@ -24,7 +22,6 @@ function addNewServiceDependency(component, source_element, source_obj, componen
         offset = 0;
         add = -20;
         stub_x = -15;
-        depedency_name = dependency_obj.type + " Use Dependency from " + source_obj.name;
     };
 
     var dependency = new Konva.Line({
@@ -167,37 +164,48 @@ function addNewServiceDependency(component, source_element, source_obj, componen
         if (e.evt.button === 0){
             // first left click
             console.log("Left clicked stub: ", source_obj.name);
-            // check if source stub is a provide dependency
+            // check if stub is a provide
             if(source_obj.type == 'Place'){
                 provide_component_obj = component_obj;
                 provide_source_obj = source_obj;
                 provide_stub_konva = stub;
                 provide_component_group = component_group;
                 provide_symbol = symbol;
+                provide_dependency_type = source_obj.dependency_type;
+                console.log("PROVIDE dependency type is " + provide_dependency_type);
                 // set source selected true
                 source_selected = true;
             }
         } else if (e.evt.button === 2) {
             // check if provide stub was selected prior
             if(source_selected){
+                // make sure connection is going to USE stub
                 if(source_obj.type == 'Transition'){
-                    use_component_obj = component_obj;
-                    use_source_obj = source_obj;
-                    use_stub_konva = stub;
-                    use_component_group = component_group;
-                    // Dont create connection if both stubs are from the same component
-                    if(provide_component_obj != use_component_obj){
-                        // check if arc is visible
-                        if(provide_symbol.opacity() == 0){
-                            // make it visible
-                            provide_symbol.opacity(1);
-                            use_stub_konva.opacity(1);
+                    // get the use stub depedency type
+                    use_dependency_type = source_obj.dependency_type;
+                    console.log("USE dependency type is " + use_dependency_type);
+                    // check if source stub and dest stub is the same dependency type
+                    if((provide_dependency_type == 'PROVIDE' && use_dependency_type == 'USE') || (provide_dependency_type == 'DATA_PROVIDE' && use_dependency_type == 'DATA_USE')){
+                        use_component_obj = component_obj;
+                        // Check if connection is going to a different component
+                        if(provide_component_obj != use_component_obj){
+                            use_source_obj = source_obj;
+                            use_stub_konva = stub;
+                            use_component_group = component_group;
+                            // check if arc is visible
+                            if(provide_symbol.opacity() == 0){
+                                // make it visible
+                                provide_symbol.opacity(1);
+                                use_stub_konva.opacity(1);
+                            }
+                            // create new connection here
+                            connection = addNewConnection(provide_component_obj, provide_source_obj, provide_stub_konva, provide_component_group, use_component_obj, use_source_obj, use_stub_konva, use_component_group);
+                        } else {
+                            alert("Cant create connection from " + provide_component_obj.name + " to " + use_component_obj.name);
                         }
-                        // create new connection here
-                        connection = addNewConnection(provide_component_obj, provide_source_obj, provide_stub_konva, provide_component_group, use_component_obj, use_source_obj, use_stub_konva, use_component_group);
                     } else {
-                        alert("Cant create connection from " + provide_component_obj.name + " to " + use_component_obj.name);
-                    }
+                        alert("Incompatible dependency types");
+                    }   
                 } else {
                     alert("Left click Provide dependency stub and Right click Use dependency stub to connect them");
                 }
@@ -493,6 +501,9 @@ function addNewDataDependency(component, source_element, source_obj, component_o
                 provide_stub_konva = stub;
                 provide_component_group = component_group;
                 provide_symbol = data_symbol_provide;
+                provide_dependency_type = source_obj.dependency_type;
+                console.log("PROVIDE dependency type is " + provide_dependency_type);
+                // set source selected true
                 source_selected = true;
             }
         } 
@@ -500,22 +511,30 @@ function addNewDataDependency(component, source_element, source_obj, component_o
             console.log("Right clicked stub: ", source_obj.name);
             // check if provide stub was selected prior to create connection
             if(source_selected){
-                console.log("Left stub was: ", provide_source_obj.name);
+                // check if connection is going to USE stub
                 if(source_obj.type == 'Transition'){
-                    use_component_obj = component_obj;
-                    use_source_obj = source_obj;
-                    use_stub_konva = stub;
-                    use_component_group = component_group;
-                    // Dont create connection if both stubs are from the same component
-                    if(provide_component_obj != use_component_obj){
-                        // make things visible
-                        provide_symbol.opacity(1);
-                        data_stub_use.opacity(1);
-                        // create new connection here
-                        connection = addNewConnection(provide_component_obj, provide_source_obj, provide_stub_konva, provide_component_group, use_component_obj, use_source_obj, use_stub_konva, use_component_group);
+                    // get the use stub dependency type
+                    use_dependency_type = source_obj.dependency_type;
+                    console.log("USE dependency type is " + use_dependency_type);
+                    // check if source stub and dest stub is the same dependency type
+                    if((provide_dependency_type == 'PROVIDE' && use_dependency_type == 'USE') || (provide_dependency_type == 'DATA_PROVIDE' && use_dependency_type == 'DATA_USE')){
+                        use_component_obj = component_obj;
+                        // check if provide stub component is different from use stub component
+                        if(provide_component_obj != use_component_obj){
+                            use_source_obj = source_obj;
+                            use_stub_konva = stub;
+                            use_component_group = component_group;
+                            // make things visible
+                            provide_symbol.opacity(1);
+                            data_stub_use.opacity(1);
+                            // create new connection here
+                            connection = addNewConnection(provide_component_obj, provide_source_obj, provide_stub_konva, provide_component_group, use_component_obj, use_source_obj, use_stub_konva, use_component_group);
+                        } else {
+                            alert("Cant create connection from " + provide_component_obj.name + " to " + use_component_obj.name);
+                        }
                     } else {
-                        alert("Cant create connection from " + provide_component_obj.name + " to " + use_component_obj.name);
-                    }
+                        alert("Incompatible dependency types");
+                    }        
                 } else {
                     alert("Left click Provide dependency stub and Right click Use dependency stub to connect them");
                 }
@@ -528,7 +547,7 @@ function addNewDataDependency(component, source_element, source_obj, component_o
             provide_stub_konva = null;
             use_stub_konva = null;
             source_selected = false;
-        } 
+        }
     });
 
     // stub for provide dependency
