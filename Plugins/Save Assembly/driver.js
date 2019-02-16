6038f74ae4d980ef41c3f710ce879d74d09aa0d2
Melody Pressley <h.pressley97@gmail.com>
2019-02-16 00:31:59 -0700

    Added basic save functionality to write placeholder data (no .yaml conversion yet).

--- Plugins/Save Assembly/driver.js
+++ Plugins/Save Assembly/driver.js
@@ -4,11 +4,39 @@
 const sa_electron = require('electron');
 const sa_ipcRenderer = electron.ipcRenderer;
 var sa_fs = require('fs'); // Load the File System to execute our common tasks (CRUD)
-var sa_app = electron.remote; 
+var sa_app = electron.remote;
 var sa_dialog = app.dialog;
 var sa_comp_list = component_list;
 
 ipcRenderer.on('save_assembly', function() {
-    window.alert('Save Assembly Placeholder')
+
+    saveAssembly();
     return;
+
 });
+
+function saveAssembly() {
+
+    // @todo: convert assembly -> .yaml; put .yaml in saveContent variable
+    var saveContent = 'placeholder content';
+
+    dialog.showSaveDialog(
+
+        {defaultPath: "~/*.yaml"},
+
+        function (fileName) {
+
+            if (fileName === undefined) {
+                console.log("You didn't save a file.");
+                return;
+            }
+            // fileName is a string that contains the path and filename created in the save file dialog.
+            fs.writeFile(fileName, saveContent, (err) => {
+                if (err) {
+                    alert("An error ocurred creating the file " + err.message)
+                };
+                console.log("Save successful.");
+            });
+      });
+
+};
