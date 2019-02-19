72692763974f03e983fe12921027e373d671b828
Melody Pressley <h.pressley97@gmail.com>
2019-02-19 02:29:45 -0700

    Save plugin now saves an array instead of individual objects.
    Also decomposed functions to be less messy.

--- Plugins/Save Assembly/driver.js
+++ Plugins/Save Assembly/driver.js
@@ -19,26 +19,15 @@ sa_ipcRenderer.on('save_assembly', function() {
 function saveAssembly() {
 
     // @todo: what other information do we need to save?
-    var saveContent = '';
+    var saveContent = sa_yaml.safeDump(sa_comp_list);
 
-    for(var i = 0; i < sa_comp_list.length; i++) {
-        saveContent += sa_yaml.safeDump(sa_comp_list[i]);
-        saveContent += '\n';
+    fileName = sa_dialog.showSaveDialog( {defaultPath: "~/*.yaml"} );
+
+    if (fileName === undefined) {
+        console.log("You didn't save a file.");
+        return;
     }
 
-    sa_dialog.showSaveDialog( {defaultPath: "~/*.yaml"}, function (fileName) {
-
-        if (fileName === undefined) {
-            console.log("You didn't save a file.");
-            return;
-        }
-
-        sa_fs.writeFile(fileName, saveContent, (err) => {
-            if (err) {
-                alert("An error ocurred creating the file " + err.message)
-            };
-            console.log("Save successful.");
-        });
-  });
+    sa_fs.writeFileSync(fileName, saveContent);
 
 };
