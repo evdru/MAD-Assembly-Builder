5d5a3bd33899057cff4512bdb2ee9a33e80eb9f8
Melody Pressley <h.pressley97@gmail.com>
2019-02-25 02:54:58 -0700

    Made saving plugin more modular & clean via function separation.

--- Plugins/Save Assembly/driver.js
+++ Plugins/Save Assembly/driver.js
@@ -22,78 +22,7 @@ function saveAssembly() {
     for(var i = 0; i < component_list.length; i++) {
 
         current_component = component_list[i];
-        scaleX = current_component.konva_component.scaleX();
-        scaleY = current_component.konva_component.scaleY();
-
-        save_place_list = [];
-        save_transition_list = [];
-
-        // saving each place in a component
-        for(var j = 0; j < current_component.place_list.length; j++) {
-            current_place = current_component.place_list[j];
-            save_place_obj = {
-                type: current_place.type,
-                name: current_place.name,
-                index: current_place.index,
-                transition_count: current_place.transition_count,
-                dependency: current_place.dependency,
-                dependency_type: current_place.dependency_type,
-                provide_dependency_list: current_place.provide_dependency_list,
-                posX: current_place.konva_place.x(),
-                posY: current_place.konva_place.y()
-            }
-            save_place_list[j] = save_place_obj;
-        }
-
-        // saving each transition in a component
-        for(var j = 0; j < current_component.transition_list.length; j++) {
-            current_transition = current_component.transition_list[j];
-            // defining src place
-            save_src_obj = {
-                type: current_transition.src.type,
-                name: current_transition.src.name,
-                index: current_transition.src.index,
-                transition_count: current_transition.src.transition_count,
-                dependency: current_transition.src.dependency,
-                dependency_type: current_transition.src.dependency_type,
-                provide_dependency_list: current_transition.src.provide_dependency_list
-            }
-            // defining dest place
-            save_dest_obj = {
-                type: current_transition.dest.type,
-                name: current_transition.dest.name,
-                index: current_transition.dest.index,
-                transition_count: current_transition.dest.transition_count,
-                dependency: current_transition.dest.dependency,
-                dependency_type: current_transition.dest.dependency_type,
-                provide_dependency_list: current_transition.dest.provide_dependency_list
-            }
-            save_transition_obj = {
-                type: current_transition.type,
-                name: current_transition.name,
-                src: save_src_obj,
-                dest: save_dest_obj,
-                func: current_transition.func,
-                dependency: current_transition.dependency,
-                dependency_type: current_transition.dependency_type,
-                use_dependency_list: current_transition.use_dependency_list
-            }
-            save_transition_list[j] = save_transition_obj;
-        }
-
-        save_component_obj = {
-            type: current_component.type,
-            name: current_component.name,
-            place_list: save_place_list,
-            transition_list: save_transition_list,
-            transition_dictionary: current_component.transition_dictionary,
-            dependency_list: current_component.dependency_list,
-            posX: current_component.group.x(),
-            posY: current_component.group.y(),
-            scaleX: current_component.konva_component.scaleX(),
-            scaleY: current_component.konva_component.scaleY()
-        }
-
+        save_component_obj = componentToSaveObj(current_component);
         sa_comp_list[i] = save_component_obj;
 
     }
@@ -110,5 +39,74 @@ function saveAssembly() {
     console.log(sa_comp_list);
 
     sa_fs.writeFileSync(fileName, saveContent);
+
+};
+
+function placeToSaveObj(place) {
+    return {
+        type: place.type,
+        name: place.name,
+        index: place.index,
+        transition_count: place.transition_count,
+        dependency: place.dependency,
+        dependency_type: place.dependency_type,
+        provide_dependency_list: place.provide_dependency_list,
+        posX: place.konva_place.x(),
+        posY: place.konva_place.y()
+    };
+};
+
+function transitionToSaveObj(transition) {
+
+    save_src_obj = placeToSaveObj(transition.src);
+    save_dest_obj = placeToSaveObj(transition.dest);
+
+    return {
+        type: transition.type,
+        name: transition.name,
+        src: save_src_obj,
+        dest: save_dest_obj,
+        func: transition.func,
+        dependency: transition.dependency,
+        dependency_type: transition.dependency_type,
+        use_dependency_list: transition.use_dependency_list
+    };
+
+};
+
+function componentToSaveObj(component) {
+
+    scaleX = component.konva_component.scaleX();
+    scaleY = component.konva_component.scaleY();
+
+    save_place_list = [];
+    save_transition_list = [];
+
+    // saving each place in a component
+    for(var j = 0; j < component.place_list.length; j++) {
+        current_place = component.place_list[j];
+        save_place_obj = placeToSaveObj(current_place);
+        save_place_list[j] = save_place_obj;
+    }
+
+    // saving each transition in a component
+    for(var j = 0; j < component.transition_list.length; j++) {
+        current_transition = component.transition_list[j];
+        save_transition_obj = transitionToSaveObj(current_transition);
+        save_transition_list[j] = save_transition_obj;
+    }
+
+    return {
+        type: component.type,
+        name: component.name,
+        place_list: save_place_list,
+        transition_list: save_transition_list,
+        transition_dictionary: component.transition_dictionary,
+        dependency_list: component.dependency_list,
+        posX: component.group.x(),
+        posY: component.group.y(),
+        scaleX: component.konva_component.scaleX(),
+        scaleY: component.konva_component.scaleY()
+    };
 
 };
