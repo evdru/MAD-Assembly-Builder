8650c5a9af46b9490199b88e4c186921c932c006
Evan Russell <edr62@nau.edu>
2019-03-02 21:09:46 -0700

    SimulateDeployment driver created, tokens created at each components first place

--- Plugins/Simulate Deployment/driver.js
+++ Plugins/Simulate Deployment/driver.js
@@ -10,19 +10,49 @@ var sd_comp_list = component_list;
 var sd_con_list = connection_list;
 
 sd_ipcRenderer.on('simulate_deployment', function() {
-    
-    for (var i = 0; i < sd_comp_list.length; i++) {
-        createTokens(sd_comp_list[i]);
-    };
-    return;
 
+    // check if component exists
+    if(sd_comp_list.length > 0) {
+        console.log("Creating and adding animation layer to stage...");
+        // animation layer
+        var animLayer = new Konva.Layer();
+        stage.add(animLayer);
+    } else {
+        console.log("Assembly has no components... exiting.");
+        return;
+    }
+    // create a konva group for tokens
+    var tokenGroup = new Konva.Group();
+    
+    // for every component
+    for (var i = 0; i < sd_comp_list.length; i++) {
+        if (sd_comp_list[i].place_list.length > 0){
+            // create a token for every component
+            createTokens(sd_comp_list[i], tokenGroup);
+        } else {
+            console.log(sd_comp_list[i].name + " did not have a place!");
+        } 
+    };
+    animLayer.add(tokenGroup);
+    animLayer.draw();
+    return;
 });
 
-function createTokens(component){
-    if (confirm("Press a button!")) {
-        txt = "You pressed OK!";
-    } else {
-    txt = "You pressed Cancel!";
-    }
-    console.log(component.name);
+function createTokens(component, tokenGroup){
+    // check if current component has first place
+    var tokenPos = component.place_list[0].place_konva.getAbsolutePosition();
+    // create a token on the first place
+    console.log("Creating token for " + component.name + " at " + component.place_list[0].name);
+    console.log("Place 1 position is " + tokenPos.x + " " + tokenPos.y);
+    var token = new Konva.Circle({
+        x: tokenPos.x,
+        y: tokenPos.y,
+        radius: 8,
+        fill: 'red',
+        stroke: 'black',
+        strokeWidth: 1,
+        opacity: 1
+    });
+    tokenGroup.add(token);
+    //component.component_group_konva.add(token);
 }
