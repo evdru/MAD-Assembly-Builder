61504e8630ce1c9b7f3b51e08a866492cce4649a
Evan Russell <edr62@nau.edu>
2019-03-04 16:06:43 -0700

    Sim mode now turns event listeners off, Created reset btn, Edit Btn to turn event listenrs back on

--- Plugins/Simulate Deployment/driver.js
+++ Plugins/Simulate Deployment/driver.js
@@ -8,6 +8,16 @@ var sd_app = electron.remote;
 var sd_dialog = app.dialog;
 var sd_comp_list = component_list;
 var sd_con_list = connection_list;
+var token_list = [];
+var simulator_mode = true;
+
+class Token{
+    constructor(name, start_position){
+        this.name = name;
+        this.start_position;
+        this.konva_circle;
+    }
+}
 
 sd_ipcRenderer.on('simulate_deployment', function() {
 
@@ -33,14 +43,40 @@ sd_ipcRenderer.on('simulate_deployment', function() {
     // create pause button
     pauseLabel = createPauseButton();
     animLayer.add(pauseLabel);
+    // create reset button
+    resetLabel = createResetButton();
+    animLayer.add(resetLabel);
+    // create edit mode button
+    editButton = createEditModeButton();
+    animLayer.add(editButton);
+
+    editButton.on('click', function(e){
+        // for every component
+        for (var i = 0; i < sd_comp_list.length; i++) {
+            setListening(sd_comp_list[i]);
+            simulator_mode = false;
+        }
+        console.log("clicked on edit mode label");
+    });
+
+    simulatorLabel.on('click', function(e){
+        // for every component
+        for (var i = 0; i < sd_comp_list.length; i++) {
+            setNotListening(sd_comp_list[i]);
+            simulator_mode = true;
+        }
+        console.log("clicked on simulator mode label");
+    });
     
     // for every component
     for (var i = 0; i < sd_comp_list.length; i++) {
         // check if component has places
         if (sd_comp_list[i].place_list.length > 0){
+            // set not listening 
+            setNotListening(sd_comp_list[i]);
             // create a token for every component
-            var token = createTokens(sd_comp_list[i], playLabel, pauseLabel, animLayer);
-
+            var place_num = 0;
+            var token = createTokens(sd_comp_list[i], playLabel, pauseLabel, resetLabel, place_num, animLayer);
         } else {
             console.log(sd_comp_list[i].name + " did not have a place!");
         } 
@@ -50,28 +86,6 @@ sd_ipcRenderer.on('simulate_deployment', function() {
     animLayer.draw();
     return;
 });
-
-function createSimulatorLabel(){
-    // simulator label
-    var simulatorLabel = new Konva.Label({
-        x: 400,
-        y: 10,
-        opacity: 1
-    });
-
-    simulatorLabel.add(new Konva.Tag({
-        fill: 'white'
-    }));
-
-    simulatorLabel.add(new Konva.Text({
-        text: 'SIMULATOR MODE',
-        fontFamily: 'Calibri',
-        fontSize: 36,
-        padding: 5,
-        fill: 'black'
-    }));
-    return simulatorLabel;
-}
 
 function createPlayButton(){
      // play label
@@ -117,16 +131,94 @@ function createPauseButton(){
     return pauseLabel;
 }
 
-function createTokens(component, playLabel, pauseLabel, animLayer){
+function createResetButton(){
+    // reset label
+    var resetLabel = new Konva.Label({
+       x: 375,
+       y: 10,
+       opacity: 1
+   });
+
+   resetLabel.add(new Konva.Tag({
+       fill: 'BLUE',
+       stroke: 'black',
+       strokeWidth: 2
+   }));
+
+   resetLabel.add(new Konva.Text({
+       text: 'RESET',
+       fontFamily: 'Calibri',
+       fontSize: 36,
+       padding: 5,
+       fill: 'black'
+   }));
+   return resetLabel;
+}
+
+function createSimulatorLabel(){
+    // simulator label
+    var simulatorLabel = new Konva.Label({
+        x: 520,
+        y: 10,
+        opacity: 1
+    });
+
+    simulatorLabel.add(new Konva.Tag({
+        fill: 'white'
+    }));
+
+    simulatorLabel.add(new Konva.Text({
+        text: 'SIMULATOR MODE',
+        fontFamily: 'Calibri',
+        fontSize: 36,
+        padding: 5,
+        fill: 'black'
+    }));
+    return simulatorLabel;
+}
+
+function createEditModeButton(){
+    // edit mode label
+    var editLabel = new Konva.Label({
+        x: 900,
+        y: 10,
+        opacity: 1
+    });
+
+    editLabel.add(new Konva.Tag({
+        fill: 'white'
+    }));
+
+    editLabel.add(new Konva.Text({
+        text: 'Go back to EDIT mode',
+        fontFamily: 'Calibri',
+        fontSize: 36,
+        padding: 5,
+        fill: 'black'
+    }));
+    return editLabel;
+}
+
+function setNotListening(component){
+    component.component_group_konva.listening(false);
+    layer.drawHit();
+}
+
+function setListening(component){
+    component.component_group_konva.listening(true);
+    layer.drawHit();
+}
+
+function createTokens(component, playLabel, pauseLabel, resetLabel, place_num, animLayer){
     // check if current component has first place
     var tokenPos = component.place_list[0].place_konva.getAbsolutePosition();
-    // create a token on the first place
+
     console.log("Creating tokens for " + component.name + " at " + component.place_list[0].name);
     console.log("Place 1 position is " + tokenPos.x + " " + tokenPos.y);
     var tokenColor = getRandomColor();
 
     // create token for every outbound transition
-    for (var tran_num = 0; tran_num < component.place_list[0].transition_outbound_list.length; tran_num++){
+    for (var tran_num = 0; tran_num < component.place_list[place_num].transition_outbound_list.length; tran_num++){
         var token = new Konva.Circle({
             x: tokenPos.x,
             y: tokenPos.y,
@@ -134,21 +226,27 @@ function createTokens(component, playLabel, pauseLabel, animLayer){
             fill: tokenColor,
             opacity: 1
         });
+
+        // create token obj
+        var token_obj = new Token(token_list.length + 1, tokenPos);
+        // add konva cricle ref to token obj
+        token_obj.konva_circle = token;
+        // add konva token to animLayer
         animLayer.add(token);
 
         var transition = component.place_list[0].transition_outbound_list[0].tran_konva;
         var tran_pos = transition.getAbsolutePosition();
         var dest_pos_x = tran_pos.x + transition.points()[2];
         var dest_post_y = tran_pos.y + transition.points()[3];
-        moveToken(token, dest_pos_x, dest_post_y, playLabel, pauseLabel);
+        //moveToken(token, dest_pos_x, dest_post_y, playLabel, pauseLabel);
         var dest_pos_x = tran_pos.x + transition.points()[4];
         var dest_post_y = tran_pos.y + transition.points()[5];
-        moveToken(token, dest_pos_x, dest_post_y, playLabel, pauseLabel);
+        moveToken(token, dest_pos_x, dest_post_y, playLabel, pauseLabel, resetLabel);
     }
     return token;
 }
 
-function moveToken(token, dest_pos_x, dest_post_y, playLabel, pauseLabel){
+function moveToken(token, dest_pos_x, dest_post_y, playLabel, pauseLabel, resetLabel){
     // the tween has to be created after the node has been added to the layer
     var tween = new Konva.Tween({
         node: token,
@@ -157,8 +255,34 @@ function moveToken(token, dest_pos_x, dest_post_y, playLabel, pauseLabel){
         y: dest_post_y,
         opacity: 1,
         // onFinish: function() {
-        //     console.log('tween finished!');
+        //     tween.destroy();
         // }
+    });
+
+    playLabel.on('click', function(e){
+        tween.play();
+        console.log("clicked on play label");
+    });
+
+    pauseLabel.on('click', function(e){
+        tween.pause();
+        console.log("clicked on pause label");
+    });
+
+    resetLabel.on('click', function(e){
+        token.position({x: token.x, y: token.y});
+        console.log("clicked on reset label");
+    });
+}
+
+function finishTransition(token, dest_pos_x, dest_post_y, playLabel, pauseLabel){
+    // the tween has to be created after the node has been added to the layer
+    var tween = new Konva.Tween({
+        node: token,
+        duration: 3,
+        x: dest_pos_x,
+        y: dest_post_y,
+        opacity: 1,
     });
 
     playLabel.on('click', function(e){
