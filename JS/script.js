621b69d38e1e7e992c9d1ff6a339fdbbf74158d8
Kyle Krueger <ktk57@nau.edu>
2019-05-05 10:44:48 -0700

    Added zoom in and out functionality

--- JS/script.js
+++ JS/script.js
@@ -124,6 +124,32 @@ function initialize() {
     layer = new Konva.Layer();
     stage.add(layer);
     var container = stage.container();
+
+    var scaleBy = 1.01;
+    stage.on('wheel', e => {
+      e.evt.preventDefault();
+      var oldScale = stage.scaleX();
+
+      var mousePointTo = {
+        x: stage.getPointerPosition().x / oldScale - stage.x() / oldScale,
+        y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale
+      };
+
+      var newScale =
+        e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+      stage.scale({ x: newScale, y: newScale });
+
+      var newPos = {
+        x:
+          -(mousePointTo.x - stage.getPointerPosition().x / newScale) *
+          newScale,
+        y:
+          -(mousePointTo.y - stage.getPointerPosition().y / newScale) *
+          newScale
+      };
+      stage.position(newPos);
+      stage.batchDraw();
+    });
 };
 
 // Drag N Drop Functions
